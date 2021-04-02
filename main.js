c0fa5158ddda6419c8d809b1e71f64e15edff99a
Virej Dasani <dasanivirej@gmail.com>
2021-04-02 18:42:03 +0530

    Changed placeholders

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function showPreview() {
-    var htmlCode = document.getElementById("htmlCode").value;
-    var cssCode = document.getElementById("cssCode").value;
-    var jsCode = document.getElementById("jsCode").value;
-    var frame = document.getElementById("preview-window").contentWindow.document;
-    frame.open();
-    frame.write(htmlCode + "<style>" + cssCode + "</style>" + "<script>" + jsCode + "</script>");
-    frame.close();
+    var htmlCode = document.getElementById("htmlCode").value
+    var cssCode = "<style>" + document.getElementById("cssCode").value + "</style>"
+    var jsCode = "<script>" + document.getElementById("jsCode").value + "</script>"
+    var frame = document.getElementById("preview-window").contentWindow.document
+    frame.open()
+    frame.write(htmlCode + cssCode + jsCode)
+    frame.close()
 }
