8e494e57689d2511cdfbd269642503cd0ec9505a
Virej Dasani <dasanivirej@gmail.com>
2021-04-03 10:38:35 +0530

    Added monaco-editor

--- main.js
+++ main.js
@@ -7,3 +7,46 @@ function showPreview() {
     frame.write(htmlCode + cssCode + jsCode)
     frame.close()
 }
+
+require.config({
+    paths: {
+        'vs': 'https://unpkg.com/monaco-editor@latest/min/vs'
+    }
+});
+window.MonacoEnvironment = {
+    getWorkerUrl: () => proxy
+};
+
+let proxy = URL.createObjectURL(new Blob([`
+	self.MonacoEnvironment = {
+		baseUrl: 'https://unpkg.com/monaco-editor@latest/min/'
+	};
+	importScripts('https://unpkg.com/monaco-editor@latest/min/vs/base/worker/workerMain.js');
+`], {
+    type: 'text/javascript'
+}));
+
+// This is monaco editor, used in vs code 
+require(["vs/editor/editor.main"], function () {
+    let editor = monaco.editor.create(document.getElementById('containerHTML'), {
+        value: '',
+        language: 'html',
+        theme: 'vs-dark'
+    });
+});
+
+require(["vs/editor/editor.main"], function () {
+    let editor = monaco.editor.create(document.getElementById('containerCSS'), {
+        value: '',
+        language: 'css',
+        theme: 'vs-dark'
+    });
+});
+
+require(["vs/editor/editor.main"], function () {
+    let editor = monaco.editor.create(document.getElementById('containerJS'), {
+        value: '',
+        language: 'javascript',
+        theme: 'vs-dark'
+    });
+});
