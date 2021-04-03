1554bb7ad2a369ed382c4a90c0dfcda694e42879
Virej Dasani <dasanivirej@gmail.com>
2021-04-03 15:26:05 +0530

    Separated app and website

---
+++ website/index.js
@@ -0,0 +1,153 @@
+// Modules to control application life and create native browser window
+const {app, BrowserWindow, Menu } = require('electron')
+const path = require('path')
+
+function createWindow () {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 850,
+    height: 550,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html')
+  // To make it occupy the whole screen
+  mainWindow.maximize()
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  createWindow()
+  
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+
+// This is for custom menu bar
+const isMac = process.platform === 'darwin'
+
+const template = [
+  // { role: 'appMenu' }
+  ...(isMac ? [{
+    label: app.name,
+    submenu: [
+      { role: 'about' },
+      { type: 'separator' },
+      { role: 'services' },
+      { type: 'separator' },
+      { role: 'hide' },
+      { role: 'hideothers' },
+      { role: 'unhide' },
+      { type: 'separator' },
+      { role: 'quit' }
+    ]
+  }] : []),
+  // { role: 'fileMenu' }
+  {
+    label: 'File',
+    submenu: [
+      isMac ? { role: 'close' } : { role: 'quit' }
+    ]
+  },
+  // { role: 'editMenu' }
+  {
+    label: 'Edit',
+    submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+      ...(isMac ? [
+        { role: 'pasteAndMatchStyle' },
+        { role: 'delete' },
+        { role: 'selectAll' },
+        { type: 'separator' },
+        {
+          label: 'Speech',
+          submenu: [
+            { role: 'startSpeaking' },
+            { role: 'stopSpeaking' }
+          ]
+        }
+      ] : [
+        { role: 'delete' },
+        { type: 'separator' },
+        { role: 'selectAll' }
+      ])
+    ]
+  },
+  // { role: 'viewMenu' }
+  {
+    label: 'View',
+    submenu: [
+      { role: 'reload' },
+      { role: 'forceReload' },
+      { role: 'toggleDevTools' },
+      { type: 'separator' },
+      { role: 'resetZoom' },
+      { role: 'zoomIn' },
+      { role: 'zoomOut' },
+      { type: 'separator' },
+      { role: 'togglefullscreen' }
+    ]
+  },
+  // { role: 'windowMenu' }
+  {
+    label: 'Window',
+    submenu: [
+      { role: 'minimize' },
+      { role: 'zoom' },
+      ...(isMac ? [
+        { type: 'separator' },
+        { role: 'front' },
+        { type: 'separator' },
+        { role: 'window' }
+      ] : [
+        { role: 'close' }
+      ])
+    ]
+  },
+  {
+    role: 'about',
+    submenu: [
+      {
+        label: 'Developed by Virej Dasani',
+        click: async () => {
+          const { shell } = require('electron')
+          await shell.openExternal('https://virejdasani.github.io/virej/')
+        },
+      },
+      {
+      label: 'CodeBox on GitHub',
+        click: async () => {
+          const { shell } = require('electron')
+          await shell.openExternal('https://github.com/virejdasani/CodeBox')
+        },
+      }
+    ]
+  }
+]
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
---
+++ website/main.js
@@ -0,0 +1,65 @@
+// For monaco editor
+require.config({
+    paths: {
+        'vs': 'https://unpkg.com/monaco-editor@latest/min/vs'
+    }
+});
+window.MonacoEnvironment = {
+    getWorkerUrl: () => proxy
+};
+
+let proxy = URL.createObjectURL(new Blob([`
+	self.MonacoEnvironment = {
+		baseUrl: 'https://unpkg.com/monaco-editor@latest/min/'
+	};
+	importScripts('https://unpkg.com/monaco-editor@latest/min/vs/base/worker/workerMain.js');
+`], {
+    type: 'text/javascript'
+}));
+
+// These are predefined because we need to get their values
+var editorHTML
+var editorCSS
+var editorJS
+
+// These three below are for the three editors
+// For HTML
+require(["vs/editor/editor.main"], function () {
+    editorHTML = monaco.editor.create(document.getElementById('containerHTML'), {
+        value: '<!-- HTML -->',
+        language: 'html',
+        theme: 'vs-dark'
+    });
+});
+// For CSS
+require(["vs/editor/editor.main"], function () {
+    editorCSS = monaco.editor.create(document.getElementById('containerCSS'), {
+        value: '/* CSS */',
+        language: 'css',
+        theme: 'vs-dark'
+    });
+});
+// For JS
+require(["vs/editor/editor.main"], function () {
+    editorJS = monaco.editor.create(document.getElementById('containerJS'), {
+        value: '// JavaScript',
+        language: 'javascript',
+        theme: 'vs-dark'
+    });
+});
+
+// To render the code in the iframe
+function showPreview() {
+    var htmlCode = editorHTML.getValue()
+    var cssCode = "<style>" + editorCSS.getValue() + "</style>"
+    var jsCode = "<script>" + editorJS.getValue() + "</script>"
+    var frame = document.getElementById("preview-window").contentWindow.document
+    frame.open()
+    frame.write(htmlCode + cssCode + jsCode)
+    frame.close()
+}
+
+// Check if online or offline
+if (navigator.onLine === false) {
+    window.location.replace("./offline/index.html");
+}
